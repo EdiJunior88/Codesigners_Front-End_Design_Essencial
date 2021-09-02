e1ea81b531bdd2a821cc4dec4550c729b4e603c8
EdiJunior88 <er.moura88@yahoo.com.br>
2021-09-02 10:35:57 -0300

    Array - Parte 1

---
+++ secao_5/Aula_16/index.js
@@ -0,0 +1,11 @@
+var listaFrutas = document.getElementById('frutas');
+
+var frutas = ["Laranja", "Kiwi", "Morango"];
+
+frutas.push("Uva");
+
+console.log(frutas)
+
+frutas.pop();
+
+console.log(frutas)
