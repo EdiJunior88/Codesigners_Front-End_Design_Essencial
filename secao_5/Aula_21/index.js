ccdc18ec895c5754167acd9806a1b1412f7945ec
EdiJunior88 <er.moura88@yahoo.com.br>
2021-11-03 21:34:57 -0300

    Loops

---
+++ secao_5/Aula_21/index.js
@@ -0,0 +1,21 @@
+var listaFrutas = document.getElementById('lista');
+
+var frutas = ["Laranja", "Kiwi", "Morango", "Abacaxi", "Melancia"];
+
+var usuario = {
+  "nome": "Brunão", 
+  "profissão": "Designer", 
+  "idade": 31
+};
+
+var myList = [];
+
+//For
+
+//For In
+
+//While
+
+//Map
+
+listaFrutas.insertAdjacentElement('beforeend', myList.join(''));
