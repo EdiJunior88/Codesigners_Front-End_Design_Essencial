87fd3e7f0feb8d68ab3b567e08938ec683d57779
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-20 20:36:21 -0300

    Preloader - Fazendo o Javascript da tela de preloader

--- Projeto_Final/HTML+CSS+JS/JS/main.js
+++ Projeto_Final/HTML+CSS+JS/JS/main.js
@@ -1,5 +1,14 @@
+//Declarando variáveis
 let btnContact = document.querySelector(".jl-btn-contact");
 
+//Página de Loading
+window.addEventListener("load", function () {
+  let pagePreloader = this.document.querySelector(".jl-preloader");
+
+  pagePreloader.classList.add("jl-fade-out");
+});
+
+//Abrindo e fechando modal (contato)
 btnContact.addEventListener("click", function () {
   let boxContact = document.querySelector(".jl-contact-info");
   boxContact.classList.toggle("jl-is-open");
