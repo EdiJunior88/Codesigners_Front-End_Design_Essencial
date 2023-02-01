7343143129705c7d59a0206345e5329bbcc38dad
EdiJunior88 <er.moura88@yahoo.com.br>
2023-01-31 21:05:59 -0300

    Portfolio Home (Slider) - Passando larguras dinamicamente para os blocos

--- Projeto_Final/HTML+CSS+JS/JS/main.js
+++ Projeto_Final/HTML+CSS+JS/JS/main.js
@@ -1,43 +1,73 @@
 //Declarando variáveis
-let btnContact = document.querySelector(".jl-btn-contact");
-let toggleModal = document.querySelectorAll(".jl-toggle-modal");
+let btnContact = document.querySelector('.jl-btn-contact');
+let toggleModal = document.querySelectorAll('.jl-toggle-modal');
 
-//Página de Loading
-window.addEventListener("load", function () {
-  let pagePreloader = this.document.querySelector(".jl-preloader");
-  pagePreloader.classList.add("jl-fade-out");
+//Page Preloader
+window.addEventListener('load', function () {
+    let pagePreloader = document.querySelector('.jl-preloader');
+    pagePreloader.classList.add('jl-fade-out');
 
-  setTimeout(function () {
-    pagePreloader.style.display = "none";
-  }, 2000);
+    setTimeout(function () {
+        pagePreloader.style.display = 'none';
+    }, 2000);
 });
 
-//Abrindo e fechando modal (Contato)
-btnContact.addEventListener("click", function () {
-  let boxContact = document.querySelector(".jl-contact-info");
-  boxContact.classList.toggle("jl-is-open");
-  this.classList.toggle("jl-change-icon");
+
+//Abrindo e Fechando Informações de Contato
+btnContact.addEventListener('click', function () {
+    let boxContact = document.querySelector('.jl-contact-info');
+    boxContact.classList.toggle('jl-is-open');
+    this.classList.toggle('jl-change-icon');
 });
 
-//Abrindo e fechando modal (Orçamento)
+//Abrindo e Fechando o Modal de Orcamento
+
 for (let i = 0; i < toggleModal.length; i++) {
-  toggleModal[i].addEventListener("click", function () {
-    let overlay = document.querySelector(".jl-overlay");
-    let modalOrcamento = document.querySelector("#jl-modal-orcamento");
+    toggleModal[i].addEventListener('click', function () {
+        let overlay = document.querySelector('.jl-overlay');
+        let modalOrcamento = document.querySelector('#jl-modal-orcamento');
 
-    overlay.classList.toggle("jl-is-open");
-    modalOrcamento.classList.toggle("jl-is-open");
-    modalOrcamento.classList.toggle("jl-slide-top-in");
-  });
+        overlay.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-is-open');
+        modalOrcamento.classList.toggle('jl-slide-top-in');
+
+    });
 }
 
-//Animando Elementos onScroll com Waypoints
-let myScrollDown = document.querySelector(".jl-scroll-down");
-
+// Animando Elementos on Scroll com Waypoints
+let myScrollDown = document.querySelector('.jl-scroll-down');
 let waypoint = new Waypoint({
-  element: myScrollDown,
-  handler: function () {
-    myScrollDown.classList.toggle("jl-fade-out");
-  },
-  offset: "80%",
+    element: myScrollDown,
+    handler: function () {
+        myScrollDown.classList.toggle('jl-fade-out');
+    },
+    offset: '80%'
 });
+
+//PORTFOLIO SLIDER
+
+//Declarando variáveis do slider
+let sliderContainer = document.querySelector('.jl-slider-container');
+let sliderList = document.querySelector('.jl-slider-list');
+let sliderItem = document.querySelectorAll('.jl-slider-item');
+let sliderListWidth = null;
+
+//Capturando larguras individuais
+let containerWidth = sliderContainer.parentElement.offsetWidth;
+
+//Passando larguras dinâmicas
+sliderContainer.style.width = containerWidth + 'px';
+
+for (let p = 0; p < sliderItem.length; p++) {
+    sliderItem[p].style.width = containerWidth + 'px';
+    let sliderItemWidth = sliderItem[p].offsetWidth;
+
+    sliderListWidth += sliderItemWidth;
+    console.log(sliderListWidth);
+}
+
+sliderList.style.width = sliderListWidth + 'px';
+
+
+//Fazendo Animação do Slider onClick
+
